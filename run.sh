e371a354513cffdc1419f8e46c2c480e0b3c5721
DerEchteFeuerpfeil <feuerpfeil.moritz@gmail.com>
2025-01-19 17:15:37 +0100

    extends run script for LLM key transfer to container

--- run.sh
+++ run.sh
@@ -29,5 +29,26 @@ case "$1" in
         ;;
 esac
 
+case "$2" in
+    openai)
+        EXPORT_ENV_VAR="OPENAI_API_KEY"
+        echo "Injecting OPENAI_API_KEY into the container."
+        ;;
+    google)
+        EXPORT_ENV_VAR="GOOGLE_API_KEY"
+        echo "Injecting GOOGLE_API_KEY into the container."
+        ;;
+    gemini)
+        EXPORT_ENV_VAR="GEMINI_API_KEY"
+        echo "Injecting GEMINI_API_KEY into the container."
+        ;;
+    *)
+        EXPORT_ENV_VAR=""
+        echo "Running container without LLM post-processing. To change, rerun this script with one of [openai, google, gemini] which injects the corresponding API key into the container."
+        exit 1
+        ;;
+esac
+
 # Run the Docker container
-docker run --rm -it -p 8000:8000 $DOCKER_ARGS $VOLUME_MOUNT $IMAGE_NAME /bin/bash -c "echo 'source /home/mineru/mineru_venv/bin/activate' >> ~/.bashrc && exec bash"
+# docker run --rm -it -p 8000:8000 $DOCKER_ARGS $VOLUME_MOUNT $IMAGE_NAME /bin/bash -c "echo 'source /home/mineru/mineru_venv/bin/activate' >> ~/.bashrc && exec bash"
+docker run --rm -it -p 8000:8000 -e $EXPORT_ENV_VAR $DOCKER_ARGS $VOLUME_MOUNT $IMAGE_NAME /bin/bash -c "echo 'source /home/mineru/mineru_venv/bin/activate' >> ~/.bashrc && exec bash"
